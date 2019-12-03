b1e69f1eed854f43799ef221808149809176a31c
sundharthiru <sundhar102000@gmail.com>
2019-12-03 18:51:51 +0530

    dbconnection

--- addcustomer.php
+++ addcustomer.php
@@ -1,17 +1,50 @@
 <?php
 include_once 'header.php';
 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "php";
+
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
+$email = $_POST['email'];
+$pass = $_POST['password'];
+$conn = new mysqli($php, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if($email!='' && $fname!=''){
+  $sql = "INSERT INTO customer (firstname, lastname, email,password) VALUES ('$fname', '$lname', '$email', '$pass')";
+    echo "New record created successfully";
+}
+ else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
+
 <div class="container">
   
-  <form action="">
+  <form action="post">
+    <div class="form-group">
+      <label for="firstname">firstname:</label>
+      <input type="firstname" name="firstname" class="form-control" id="firstname">
+    </div>
+    <div class="form-group">
+      <label for="lastname">lastname:</label>
+      <input type="lastname" name="lastname" class="form-control" id="lastname">
+    </div>
     <div class="form-group">
       <label for="email">Email address:</label>
-      <input type="email" class="form-control" id="email">
+      <input type="email" name="email" class="form-control" id="email">
     </div>
     <div class="form-group">
       <label for="pwd">Password:</label>
-      <input type="password" class="form-control" id="pwd">
+      <input type="password" name="password" class="form-control" id="pwd">
     </div>
     <div class="checkbox">
       <label><input type="checkbox"> Remember me</label>
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
  include 'header.php';
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,38 +11,39 @@
  
 </head>
 <body>
-	<form action="addcustomer.php" method="post">
 	<div class="container">
-  <h2>Basic Table</h2><br><br>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
+  	<form action="addcustomer.php" method="post">
 
-  <button type="submit">AddCustomer</button>
-  </form>
+    <h2>Customer Table</h2><br><br>
+    <table class="table">
+    <button class="pull-right btn btn-primary"type="submit">AddCustomer</button>
+      <thead>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>john@example.com</td>
+        </tr>
+        <tr>
+          <td>Mary</td>
+          <td>Moe</td>
+          <td>mary@example.com</td>
+        </tr>
+        <tr>
+          <td>July</td>
+          <td>Dooley</td>
+          <td>july@example.com</td>
+        </tr>
+      </tbody>
+    </table>
+
+    </form>
 </div>
 
 </body>
